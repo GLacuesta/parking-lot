29aa8dc0c118f7f737b7ccbc0998317597f29fa2
AbstractGL <lacuesta.gerry@gmail.com>
2021-02-03 11:42:50 +0800

    Added boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ParkingLotBoard from './containers/ParkingLotBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <ParkingLotBoard />
+    </div>
+  );
+}
+
+export default App;
